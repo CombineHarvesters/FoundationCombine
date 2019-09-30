f5c70a0a07178fd7802eea07c621b3910f151fe9
Daniel Tull <dt@danieltull.co.uk>
2019-09-30 21:17:57 +0100

    Add tryReduce(into:_:) for a publisher

--- Sources/FoundationCombine/ReduceInto.swift
+++ Sources/FoundationCombine/ReduceInto.swift
@@ -3,10 +3,15 @@ import Combine
 
 extension Publisher {
 
-    /// Applies a closure that accumulates each element of a stream and publishes a final result upon completion.
+    /// Applies a closure that accumulates each element of a stream and
+    /// publishes a final result upon completion.
     ///
-    /// - Parameter initialValue: The value to use as the initial accumulating value.
-    /// - Parameter updateAccumulatingResult: A closure that updates the accumulating value with the next element from the upstream publisher to produce a new value.
+    /// - Parameter initialValue: The value to use as the initial accumulating
+    ///                           value.
+    /// - Parameter updateAccumulatingResult: A closure that updates the
+    ///                                       accumulating value with the next
+    ///                                       element from the upstream
+    ///                                       publisher to produce a new value.
     public func reduce<Value>(
         into initialValue: Value,
         _ updateAccumulatingResult: @escaping (inout Value, Output) -> ()
@@ -15,6 +20,28 @@ extension Publisher {
         reduce(initialValue) { (value, output) -> Value in
             var variable = value
             updateAccumulatingResult(&variable, output)
+            return variable
+        }
+    }
+
+    /// Applies an error-throwing closure that accumulates each element of a
+    /// stream and publishes a final result upon completion.
+    ///
+    /// - Parameter initialValue: The value to use as the initial accumulating
+    ///                           value.
+    /// - Parameter updateAccumulatingResult: An error-throwing closure that
+    ///                                       updates the accumulating value
+    ///                                       with the next element from the
+    ///                                       upstream publisher to produce a
+    ///                                       new value.
+    public func tryReduce<Value>(
+        into initialValue: Value,
+        _ updateAccumulatingResult: @escaping (inout Value, Output) throws -> ()
+    ) -> Publishers.TryReduce<Self, Value> {
+
+        tryReduce(initialValue) { (value, output) -> Value in
+            var variable = value
+            try updateAccumulatingResult(&variable, output)
             return variable
         }
     }
--- Tests/FoundationCombineTests/ReduceIntoTests.swift
+++ Tests/FoundationCombineTests/ReduceIntoTests.swift
@@ -6,7 +6,7 @@ import XCTest
 
 final class ReduceIntoTests: XCTestCase {
 
-    func test() {
+    func testReduceInto() {
 
         let subject = PassthroughSubject<Int, Never>()
         let publisher = subject.reduce(into: []) { $0.append($1) }
@@ -22,6 +22,47 @@ final class ReduceIntoTests: XCTestCase {
             .subscription,
             .input([1,2,3]),
             .completion(.finished)
+        ])
+    }
+
+    func testTryReduceIntoSuccess() {
+
+        let subject = PassthroughSubject<Int, Error>()
+        let publisher = subject
+            .tryReduce(into: []) { $0.append($1) }
+            .mapError { TestError($0.localizedDescription) }
+        let subscriber = TestSubscriber<[Int], TestError>()
+
+        publisher.subscribe(subscriber) // Subscription
+        subject.send(1)
+        subject.send(2)
+        subject.send(3)
+        subject.send(completion: .finished) // [1,2,3], Completion
+
+        XCTAssertEqual(subscriber.events, [
+            .subscription,
+            .input([1,2,3]),
+            .completion(.finished)
+        ])
+    }
+
+    func testTryReduceIntoFailure() {
+
+        let subject = PassthroughSubject<Int, Error>()
+        let publisher = subject
+            .tryReduce(into: [Int]()) { (_,_) in throw TestError("Ooops!") }
+            .mapError { TestError($0.localizedDescription) }
+        let subscriber = TestSubscriber<[Int], TestError>()
+
+        publisher.subscribe(subscriber) // Subscription
+        subject.send(1)
+        subject.send(2)
+        subject.send(3)
+        subject.send(completion: .finished) // [1,2,3], Completion
+
+        XCTAssertEqual(subscriber.events, [
+            .subscription,
+            .completion(.failure("Ooops!"))
         ])
     }
 }
